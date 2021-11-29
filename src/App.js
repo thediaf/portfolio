0143bfa1db424048e089f22de474d15fc4a7c1d8
thediaf <jaharacs98@gmail.com>
2021-11-29 14:59:09 +0000

    Home

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './component/Navbar';
+import Home from './component/Home';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Home />
     </div>
   );
 }
---
+++ src/component/Home.js
@@ -0,0 +1,25 @@
+import React from "react";
+import './Home.css'
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+const Home = () => {
+    return (
+        <div class="py-5 md:py-12 px-10 grid grid-cols-3 gap-4">
+            <div className="col-span-2 dia">
+                <h1 className="text-4xl">Hi! I'm Diafra Soumare</h1>
+                <p className="text-2xl animate-pulse rotate-180 before:bg-blue-500"
+                >
+                    I'm a Full-stack web developer,<br/>
+                    a freelancer and a problem solver
+                </p>
+            </div>
+            <div className="picture">
+                <img src="images/avatar.png" alt="my picture" className="shadow-xl w-56" />
+            </div>
+        </div>
+
+        
+    );
+}
+
+export default Home;
