f69820e865a26a51ef932e032e7f3e55daa82a3c
thediaf <jaharacs98@gmail.com>
2021-11-29 21:11:59 +0000

    About

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './component/Navbar';
 import Home from './component/Home';
+import About from './component/About';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <div className="container">
       <Home />
+      <About />
+      </div>
     </div>
   );
 }
---
+++ src/component/About.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const About = () => {
+    return (
+        <section className="">
+            <div className="title text-right text-3xl">
+                about
+            </div>
+            <div class="py-5 md:py-12 px-10 grid grid-flow-row grid-cols-3 gap-4">
+                <div className="picture">
+                    <img src="images/avatar.png" alt="my picture" className="shadow-xl w-56" />
+                </div>
+                <div className="col-span-2">
+                    <p className="text-2xl  rotate-180 before:bg-blue-500"
+                    > 
+                        Hello! I'm Diafra Soumare, a passionate software engineer. 
+                        I develop web application and design database. 
+                        My core skill is based on PHP and JavaScript and I love to do most of the things using 
+                        Symfony, a PHP framework. I love to make the web more open to the world. I have graduated with 
+                        a bachelor's degree in Computer Science Engineering from Gaston Berger University at 
+                        Saint-Louis, Senegal in 2020. I am available for any kind of job opportunity that suits my interests.
+                    </p>
+                </div>
+            </div>
+            
+        </section>
+    )
+}
+
+export default About;
