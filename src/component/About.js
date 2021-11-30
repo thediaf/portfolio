5f26016c04a38a88c3f09811daac755d227ff476
thediaf <jaharacs98@gmail.com>
2021-11-30 10:42:38 +0000

    Tabs component

--- src/component/About.js
+++ src/component/About.js
@@ -1,4 +1,5 @@
 import React from "react";
+import TabsRender from "./Tabs";
 
 const About = () => {
     return (
@@ -22,6 +23,7 @@ const About = () => {
                     </p>
                 </div>
             </div>
+            <TabsRender />
             
         </section>
     )
---
+++ src/component/Tabs.js
@@ -0,0 +1,89 @@
+import React from "react";
+
+const Tabs = ({ color }) => {
+  const [openTab, setOpenTab] = React.useState(1);
+  return (
+    <>
+      <div className="flex flex-wrap">
+        <div className="">
+        <ul class="flex justify-center space-x-2">
+                <li>
+                  <a
+                    className={
+                      "text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal " +
+                      (openTab === 1
+                        ? "text-white bg-" + color
+                        : "text-" + color + " bg-white")
+                    }
+                    onClick={e => {
+                      e.preventDefault();
+                      setOpenTab(1);
+                    }}
+                    data-toggle="tab"
+                    href="#link1"
+                    role="tablist"
+                  >
+                    Experiences
+                  </a>
+                </li>
+                <li> 
+                  <a
+                  className={
+                    "text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal " +
+                    (openTab === 2
+                      ? "text-white bg-" + color
+                      : "text-" + color + " bg-white")
+                  }
+                  onClick={e => {
+                    e.preventDefault();
+                    setOpenTab(2);
+                  }}
+                  data-toggle="tab"
+                  href="#link2"
+                  role="tablist"
+                >
+                  Formations
+                </a>                
+              </li>
+                
+            </ul>
+          <div className="relative flex flex-col min-w-0 break-words bg-white w-auto items-center mb-6 ">
+            <div className="px-4 py-5 flex-auto">
+              <div className="tab-content tab-space">
+                <div className={openTab === 1 ? "block" : "hidden"} id="link1">
+                  <p>
+                    Collaboratively administrate empowered markets via
+                    plug-and-play networks. Dynamically procrastinate B2C users
+                    after installed base benefits.
+                    <br />
+                    <br /> Dramatically visualize customer directed convergence
+                    without revolutionary ROI.
+                  </p>
+                </div>
+                <div className={openTab === 2 ? "block" : "hidden"} id="link2">
+                  <p>
+                    Completely synergize resource taxing relationships via
+                    premier niche markets. Professionally cultivate one-to-one
+                    customer service with robust ideas.
+                    <br />
+                    <br />
+                    Dynamically innovate resource-leveling customer service for
+                    state of the art customer service.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default function TabsRender() {
+  return (
+    <>
+       <Tabs color="black" />;
+    </>
+  );
+}
