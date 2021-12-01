6953f2dabbf9fd82b05eb10074c1cd66292d756f
thediaf <jaharacs98@gmail.com>
2021-12-01 19:32:53 +0000

    Skills

---
+++ src/component/Skill.js
@@ -0,0 +1,102 @@
+import React from 'react';
+
+const Skill = () =>{
+    return (
+        <section className="skill">
+            <div className="title left-title pl-10 text-3xl">
+                skill
+            </div>
+            <div className="mb-3 pb-5 px-10 text-justify gap-4">
+                <p>Hello! I'm Diafra Soumare, a passionate software engineer. I develop web application and design database.</p>
+                <div className="ml-5">
+                    <span className="text-xl font-bold">Languages</span>
+                    <div className="flex ">
+                        <img src="images/php.png" alt="PHP" 
+                            className="m-7 w-24 transition duration-500 
+                            ease-in transform hover:-translate-y-4 hover:scale-110" 
+                        />
+                        <img src="images/php.png" alt="PHP" 
+                            className="shadow-lg m-7 w-24 transition duration-500 
+                            ease-in transform hover:-translate-y-4 hover:scale-110" 
+                        />
+                        <img src="images/php.png" alt="PHP" 
+                            className="m-7 w-24 transition duration-500 
+                            ease-in transform hover:-translate-y-4 hover:scale-110" 
+                        />
+                        <img src="images/php.png" alt="PHP" 
+                            className="m-7 w-24 transition duration-500 
+                            ease-in transform hover:-translate-y-4 hover:scale-110" 
+                        />
+                        
+                    </div>
+                </div>
+                <div className="ml-5">
+                    <span className="text-xl font-bold">Frameworks and libraries</span>
+                    <div className="flex">
+                        <img src="images/php.png" alt="PHP" 
+                            className="m-7 w-24 transition duration-500 
+                            ease-in transform hover:-translate-y-4 hover:scale-110" 
+                        />
+                        <img src="images/php.png" alt="PHP" 
+                            className="m-7 w-24 transition duration-500 
+                            ease-in transform hover:-translate-y-4 hover:scale-110" 
+                        />
+                        <img src="images/php.png" alt="PHP" 
+                            className="m-7 w-24 transition duration-500 
+                            ease-in transform hover:-translate-y-4 hover:scale-110" 
+                        />
+                        <img src="images/php.png" alt="PHP" 
+                            className="m-7 w-24 transition duration-500 
+                            ease-in transform hover:-translate-y-4 hover:scale-110" 
+                        />
+                    </div>
+                </div>
+                <div className="ml-5">
+                    <span className="text-xl font-bold">Databases</span>
+                    <div className="flex">
+                        <img src="images/oracle.png" alt="oracle" 
+                            className="m-7 w-24 transition duration-500 
+                                ease-in transform hover:-translate-y-3 hover:scale-110" 
+                        />
+                        <img src="images/oracle.png" alt="oracle" 
+                            className="m-7 w-24 transition duration-500 
+                                ease-in transform hover:-translate-y-3 hover:scale-110" 
+                        />
+                        <img src="images/oracle.png" alt="oracle" 
+                            className="m-7 w-24 transition duration-500 
+                                ease-in transform hover:-translate-y-3 hover:scale-110" 
+                        />
+                        <img src="images/oracle.png" alt="oracle" 
+                            className="m-7 w-24 transition duration-500 
+                                ease-in transform hover:-translate-y-3 hover:scale-110" 
+                        />
+                    </div>
+                </div>
+                <div className="ml-5">
+                    <span className="text-xl font-bold">Tools</span>
+                    <div className="flex">
+                        <img src="images/git.png" alt="git"
+                            className="m-7 w-24 transition duration-500 
+                                ease-in transform hover:-translate-y-3 hover:scale-110" 
+                        />
+                        <img src="images/git.png" alt="git"
+                            className="m-7 w-24 transition duration-500 
+                                ease-in transform hover:-translate-y-3 hover:scale-110" 
+                        />
+                        <img src="images/git.png" alt="git"
+                            className="m-7 w-24 transition duration-500 
+                                ease-in transform hover:-translate-y-3 hover:scale-110" 
+                        />
+                        <img src="images/git.png" alt="git"
+                            className="m-7 w-24 transition duration-500 
+                                ease-in transform hover:-translate-y-3 hover:scale-110" 
+                        />
+                    </div>
+                </div>
+            </div>
+            
+        </section>
+    )
+}
+
+export default Skill;
