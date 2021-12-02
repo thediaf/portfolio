cf51f738d5e83fea127ce929872b1a1c411b397d
thediaf <jaharacs98@gmail.com>
2021-12-02 13:55:18 +0000

    Footer

---
+++ src/component/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+            <div class="mt-8 w-full">
+                <hr class="border-gray-300"/>
+                <div class="w-full my-5 text-gray-800">
+                    <p>Designed and build with passion by Me</p>
+                </div>
+            </div>
+    )
+}
+
+export default Footer;
