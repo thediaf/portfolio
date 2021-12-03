f9a362aed01fbeb217f12f89b1f5c0826dae581b
thediaf <jaharacs98@gmail.com>
2021-12-03 02:20:42 +0000

    My skills section responsivity bug fixed

--- src/component/Skill.js
+++ src/component/Skill.js
@@ -10,21 +10,21 @@ const Skill = () =>{
                 <p>Hello! I'm Diafra Soumare, a passionate software engineer. I develop web application and design database.</p>
                 <div className="ml-5">
                     <span className="text-xl font-bold">Languages</span>
-                    <div className="flex ">
+                    <div className="flex flex-wrap">
                         <img src="images/php.png" alt="PHP" 
-                            className="m-7 w-24 transition duration-500 
+                            className="m-3 md:m-7 w-24 transition duration-500 
                             ease-in transform hover:-translate-y-4 hover:scale-110" 
                         />
                         <img src="images/php.png" alt="PHP" 
-                            className="shadow-lg m-7 w-24 transition duration-500 
+                            className="shadow-lg m-3 md:m-7 w-24 transition duration-500 
                             ease-in transform hover:-translate-y-4 hover:scale-110" 
                         />
                         <img src="images/php.png" alt="PHP" 
-                            className="m-7 w-24 transition duration-500 
+                            className="m-3 md:m-7 w-24 transition duration-500 
                             ease-in transform hover:-translate-y-4 hover:scale-110" 
                         />
                         <img src="images/php.png" alt="PHP" 
-                            className="m-7 w-24 transition duration-500 
+                            className="m-3 md:m-7 w-24 transition duration-500 
                             ease-in transform hover:-translate-y-4 hover:scale-110" 
                         />
                         
@@ -32,63 +32,63 @@ const Skill = () =>{
                 </div>
                 <div className="ml-5">
                     <span className="text-xl font-bold">Frameworks and libraries</span>
-                    <div className="flex">
+                    <div className="flex flex-wrap">
                         <img src="images/php.png" alt="PHP" 
-                            className="m-7 w-24 transition duration-500 
+                            className="m-3 md:m-7 w-24 transition duration-500 
                             ease-in transform hover:-translate-y-4 hover:scale-110" 
                         />
                         <img src="images/php.png" alt="PHP" 
-                            className="m-7 w-24 transition duration-500 
+                            className="m-3 md:m-7 w-24 transition duration-500 
                             ease-in transform hover:-translate-y-4 hover:scale-110" 
                         />
                         <img src="images/php.png" alt="PHP" 
-                            className="m-7 w-24 transition duration-500 
+                            className="m-3 md:m-7 w-24 transition duration-500 
                             ease-in transform hover:-translate-y-4 hover:scale-110" 
                         />
                         <img src="images/php.png" alt="PHP" 
-                            className="m-7 w-24 transition duration-500 
+                            className="m-3 md:m-7 w-24 transition duration-500 
                             ease-in transform hover:-translate-y-4 hover:scale-110" 
                         />
                     </div>
                 </div>
                 <div className="ml-5">
                     <span className="text-xl font-bold">Databases</span>
-                    <div className="flex">
+                    <div className="flex flex-wrap">
                         <img src="images/oracle.png" alt="oracle" 
-                            className="m-7 w-24 transition duration-500 
+                            className="m-3 md:m-7 w-24 transition duration-500 
                                 ease-in transform hover:-translate-y-3 hover:scale-110" 
                         />
                         <img src="images/oracle.png" alt="oracle" 
-                            className="m-7 w-24 transition duration-500 
+                            className="m-3 md:m-7 w-24 transition duration-500 
                                 ease-in transform hover:-translate-y-3 hover:scale-110" 
                         />
                         <img src="images/oracle.png" alt="oracle" 
-                            className="m-7 w-24 transition duration-500 
+                            className="m-3 md:m-7 w-24 transition duration-500 
                                 ease-in transform hover:-translate-y-3 hover:scale-110" 
                         />
                         <img src="images/oracle.png" alt="oracle" 
-                            className="m-7 w-24 transition duration-500 
+                            className="m-3 md:m-7 w-24 transition duration-500 
                                 ease-in transform hover:-translate-y-3 hover:scale-110" 
                         />
                     </div>
                 </div>
                 <div className="ml-5">
                     <span className="text-xl font-bold">Tools</span>
-                    <div className="flex">
+                    <div className="flex flex-wrap">
                         <img src="images/git.png" alt="git"
-                            className="m-7 w-24 transition duration-500 
+                            className="m-3 md:m-7 w-24 transition duration-500 
                                 ease-in transform hover:-translate-y-3 hover:scale-110" 
                         />
                         <img src="images/git.png" alt="git"
-                            className="m-7 w-24 transition duration-500 
+                            className="m-3 md:m-7 w-24 transition duration-500 
                                 ease-in transform hover:-translate-y-3 hover:scale-110" 
                         />
                         <img src="images/git.png" alt="git"
-                            className="m-7 w-24 transition duration-500 
+                            className="m-3 md:m-7 w-24 transition duration-500 
                                 ease-in transform hover:-translate-y-3 hover:scale-110" 
                         />
                         <img src="images/git.png" alt="git"
-                            className="m-7 w-24 transition duration-500 
+                            className="m-3 md:m-7 w-24 transition duration-500 
                                 ease-in transform hover:-translate-y-3 hover:scale-110" 
                         />
                     </div>
