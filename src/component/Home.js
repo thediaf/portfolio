51e076e26b18f6a311e8cde3e07a0cf56de0cc1f
thediaf <jaharacs98@gmail.com>
2021-12-05 19:36:40 +0000

    React typewriter effect add

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,17 +1,32 @@
 import React from "react";
 import './Home.css'
 // import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import TypeWriterEffect from 'react-typewriter-effect';
 
 const Home = () => {
     return (
         <div className="py-5 md:py-10 my-16 md:my-24 px-10 grid grid-cols-3 gap-4">
             <div className="col-span-2">
                 <h1 className="text-5xl md:text-7xl">Hi! I'm Diafra Soumare</h1>
-                <p className="text-xl md:text-4xl mt-5 animate-pulse rotate-180 before:bg-blue-500"
+                <div className="text-xl md:text-4xl mt-5"
                 >
-                    I'm a Full-stack web developer,<br/>
-                    a freelancer and a problem solver
-                </p>
+                    I'm a
+                <TypeWriterEffect
+                    textStyle={{
+                    textAlign: 'center'
+                    }}
+                    startDelay={2000}
+                    cursorColor="#3F3D56"
+                    multiText={[
+                        ' freelancer',
+                        ' problem solver',
+                        ' full-stack web developer',
+                    ]}
+                    loop={true}
+                    nextTextDelay={1000}
+                    typeSpeed={30}
+                />
+                </div>
             </div>
             <div className="picture">
                 <img src="images/avatar.png" alt="my picture" className="shadow-xl w-64" />
