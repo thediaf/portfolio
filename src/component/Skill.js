be5f0b593f12efb9af12c05d445951b6130a0dd9
thediaf <jaharacs98@gmail.com>
2021-12-09 02:56:56 +0000

    Skill section redesigned

--- src/component/Skill.js
+++ src/component/Skill.js
@@ -10,89 +10,120 @@ const Skill = () =>{
             </div>
             <div className="mb-3 md:ml-5 pb-5 px-10 text-justify gap-4">
                 <p>Hello! I'm Diafra Soumare, a passionate software engineer. I develop web application and design database.</p>
-                <div className="ml-5 mt-4">
+                <div className="md:ml-5 mt-4">
                     <span className="text-xl font-bold">Languages</span>
-                    <div className="flex flex-wrap my-3">
-                        <img src="images/php.png" alt="PHP" 
-                            className="m-3 md:m-7 w-24 transition duration-500 
-                            ease-in transform hover:-translate-y-4 hover:scale-110" 
-                        />
-                        <img src="images/php.png" alt="PHP" 
-                            className="shadow-lg m-3 md:m-7 w-24 transition duration-500 
-                            ease-in transform hover:-translate-y-4 hover:scale-110" 
-                        />
-                        <img src="images/php.png" alt="PHP" 
-                            className="m-3 md:m-7 w-24 transition duration-500 
-                            ease-in transform hover:-translate-y-4 hover:scale-110" 
-                        />
-                        <img src="images/php.png" alt="PHP" 
-                            className="m-3 md:m-7 w-24 transition duration-500 
-                            ease-in transform hover:-translate-y-4 hover:scale-110" 
-                        />
-                        
+                    <div className="flex flex-wrap">
+                        <div className="w-auto md:h-28 flex justify-center p-5 md:p-0 mx-3 my-5 animate-wiggle md:flex-1 bg-white shadow rounded-lg">
+                            <img src="images/php.png" alt="PHP" 
+                                className=" w-20 md:w-24 m-auto transition duration-500 
+                                ease-in transform " 
+                            />
+                        </div>
+                        <div className="w-auto md:h-28 flex justify-center p-5 md:p-0 mx-3 my-5 animate-wiggle md:flex-1 bg-white shadow rounded-lg">
+                            <img src="images/php.png" alt="PHP" 
+                                className=" w-20 md:w-24 m-auto transition duration-500 
+                                ease-in transform " 
+                            />
+                        </div>
+                        <div className="w-auto md:h-28 flex justify-center p-5 md:p-0 mx-3 my-5 animate-wiggle md:flex-1 bg-white shadow rounded-lg">
+                            <img src="images/php.png" alt="PHP" 
+                                className=" w-20 md:w-24 m-auto transition duration-500 
+                                ease-in transform " 
+                            />
+                        </div>
+                        <div className="w-auto md:h-28 flex justify-center p-5 md:p-0 mx-3 my-5 animate-wiggle md:flex-1 bg-white shadow rounded-lg">
+                            <img src="images/php.png" alt="PHP" 
+                                className=" w-20 md:w-24 m-auto transition duration-500 
+                                ease-in transform " 
+                            />
+                        </div>
                     </div>
                 </div>
-                <div className="ml-5 mt-4">
+                <div className="md:ml-5 mt-4">
                     <span className="text-xl font-bold">Frameworks and libraries</span>
                     <div className="flex flex-wrap my-3">
-                        <img src="images/php.png" alt="PHP" 
-                            className="m-3 md:m-7 w-24 transition duration-500 
-                            ease-in transform hover:-translate-y-4 hover:scale-110" 
-                        />
-                        <img src="images/php.png" alt="PHP" 
-                            className="m-3 md:m-7 w-24 transition duration-500 
-                            ease-in transform hover:-translate-y-4 hover:scale-110" 
-                        />
-                        <img src="images/php.png" alt="PHP" 
-                            className="m-3 md:m-7 w-24 transition duration-500 
-                            ease-in transform hover:-translate-y-4 hover:scale-110" 
-                        />
-                        <img src="images/php.png" alt="PHP" 
-                            className="m-3 md:m-7 w-24 transition duration-500 
-                            ease-in transform hover:-translate-y-4 hover:scale-110" 
-                        />
+                    <div className="w-auto md:h-28 flex justify-center p-5 md:p-0 mx-3 my-5 animate-wiggle md:flex-1 bg-white shadow rounded-lg">
+                            <img src="images/php.png" alt="PHP" 
+                                className=" w-20 md:w-24 m-auto transition duration-500 
+                                ease-in transform " 
+                            />
+                        </div>
+                        <div className="w-auto md:h-28 flex justify-center p-5 md:p-0 mx-3 my-5 animate-wiggle md:flex-1 bg-white shadow rounded-lg">
+                            <img src="images/php.png" alt="PHP" 
+                                className=" w-20 md:w-24 m-auto transition duration-500 
+                                ease-in transform " 
+                            />
+                        </div>
+                        <div className="w-auto md:h-28 flex justify-center p-5 md:p-0 mx-3 my-5 animate-wiggle md:flex-1 bg-white shadow rounded-lg">
+                            <img src="images/php.png" alt="PHP" 
+                                className=" w-20 md:w-24 m-auto transition duration-500 
+                                ease-in transform " 
+                            />
+                        </div>
+                        <div className="w-auto md:h-28 flex justify-center p-5 md:p-0 mx-3 my-5 animate-wiggle md:flex-1 bg-white shadow rounded-lg">
+                            <img src="images/php.png" alt="PHP" 
+                                className=" w-20 md:w-24 m-auto transition duration-500 
+                                ease-in transform " 
+                            />
+                        </div>
                     </div>
                 </div>
-                <div className="ml-5 mt-4">
+                <div className="md:ml-5 mt-4">
                     <span className="text-xl font-bold">Databases</span>
                     <div className="flex flex-wrap my-3">
-                        <img src="images/oracle.png" alt="oracle" 
-                            className="m-3 md:m-7 w-24 transition duration-500 
-                                ease-in transform hover:-translate-y-3 hover:scale-110" 
-                        />
-                        <img src="images/oracle.png" alt="oracle" 
-                            className="m-3 md:m-7 w-24 transition duration-500 
-                                ease-in transform hover:-translate-y-3 hover:scale-110" 
-                        />
-                        <img src="images/oracle.png" alt="oracle" 
-                            className="m-3 md:m-7 w-24 transition duration-500 
-                                ease-in transform hover:-translate-y-3 hover:scale-110" 
-                        />
-                        <img src="images/oracle.png" alt="oracle" 
-                            className="m-3 md:m-7 w-24 transition duration-500 
-                                ease-in transform hover:-translate-y-3 hover:scale-110" 
-                        />
+                    <div className="w-auto md:h-28 flex justify-center p-5 md:p-0 mx-3 my-5 animate-wiggle md:flex-1 bg-white shadow rounded-lg">
+                            <img src="images/oracle.png" alt="PHP" 
+                                className=" w-20 md:w-24 m-auto transition duration-500 
+                                ease-in transform " 
+                            />
+                        </div>
+                        <div className="w-auto md:h-28 flex justify-center p-5 md:p-0 mx-3 my-5 animate-wiggle md:flex-1 bg-white shadow rounded-lg">
+                            <img src="images/oracle.png" alt="PHP" 
+                                className=" w-20 md:w-24 m-auto transition duration-500 
+                                ease-in transform " 
+                            />
+                        </div>
+                        <div className="w-auto md:h-28 flex justify-center p-5 md:p-0 mx-3 my-5 animate-wiggle md:flex-1 bg-white shadow rounded-lg">
+                            <img src="images/oracle.png" alt="PHP" 
+                                className=" w-20 md:w-24 m-auto transition duration-500 
+                                ease-in transform " 
+                            />
+                        </div>
+                        <div className="w-auto md:h-28 flex justify-center p-5 md:p-0 mx-3 my-5 animate-wiggle md:flex-1 bg-white shadow rounded-lg">
+                            <img src="images/oracle.png" alt="PHP" 
+                                className=" w-20 md:w-24 m-auto transition duration-500 
+                                ease-in transform " 
+                            />
+                        </div>
                     </div>
                 </div>
-                <div className="ml-5 mt-4">
+                <div className="md:ml-5 mt-4">
                     <span className="text-xl font-bold">Tools</span>
                     <div className="flex flex-wrap my-3">
-                        <img src="images/git.png" alt="git"
-                            className="m-3 md:m-7 w-24 transition duration-500 
-                                ease-in transform hover:-translate-y-3 hover:scale-110" 
-                        />
-                        <img src="images/git.png" alt="git"
-                            className="m-3 md:m-7 w-24 transition duration-500 
-                                ease-in transform hover:-translate-y-3 hover:scale-110" 
-                        />
-                        <img src="images/git.png" alt="git"
-                            className="m-3 md:m-7 w-24 transition duration-500 
-                                ease-in transform hover:-translate-y-3 hover:scale-110" 
-                        />
-                        <img src="images/git.png" alt="git"
-                            className="m-3 md:m-7 w-24 transition duration-500 
-                                ease-in transform hover:-translate-y-3 hover:scale-110" 
-                        />
+                    <div className="w-auto md:h-28 flex justify-center p-5 md:p-0 mx-3 my-5 animate-wiggle md:flex-1 bg-white shadow rounded-lg">
+                            <img src="images/git.png" alt="PHP" 
+                                className=" w-20 md:w-24 m-auto transition duration-500 
+                                ease-in transform " 
+                            />
+                        </div>
+                        <div className="w-auto md:h-28 flex justify-center p-5 md:p-0 mx-3 my-5 animate-wiggle md:flex-1 bg-white shadow rounded-lg">
+                            <img src="images/git.png" alt="PHP" 
+                                className=" w-20 md:w-24 m-auto transition duration-500 
+                                ease-in transform " 
+                            />
+                        </div>
+                        <div className="w-auto md:h-28 flex justify-center p-5 md:p-0 mx-3 my-5 animate-wiggle md:flex-1 bg-white shadow rounded-lg">
+                            <img src="images/git.png" alt="PHP" 
+                                className=" w-20 md:w-24 m-auto transition duration-500 
+                                ease-in transform " 
+                            />
+                        </div>
+                        <div className="w-auto md:h-28 flex justify-center p-5 md:p-0 mx-3 my-5 animate-wiggle md:flex-1 bg-white shadow rounded-lg">
+                            <img src="images/git.png" alt="PHP" 
+                                className=" w-20 md:w-24 m-auto transition duration-500 
+                                ease-in transform " 
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
