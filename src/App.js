dd96fa00ff90ff8c357137648c6ad17def36d178
thediaf <jaharacs98@gmail.com>
2021-12-10 18:17:33 +0000

    Each section with his components folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import NavBar from './component/Navbar';
-import Home from './component/Home';
-import About from './component/About';
-import Skill from './component/Skill';
-import Works from './component/Works';
-import Footer from './component/Footer';
-import Contact from './component/Contact';
+import NavBar from './component/Navbar/Navbar';
+import Home from './component/Home/Home';
+import About from './component/About/About';
+import Skill from './component/Skill/Skill';
+import Works from './component/Work/Works';
+import Contact from './component/Contact/Contact';
+import Footer from './component/Footer/Footer';
 import { BrowserRouter } from 'react-router-dom';
 
 function App() {
