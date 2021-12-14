53b21cdab1833640dc5ac2a4f89779bd7d44bf6f
thediaf <jaharacs98@gmail.com>
2021-12-14 20:07:27 +0000

    Works picture

---
+++ src/component/Skill/SkillsData.js
@@ -0,0 +1,24 @@
+import php_logo from "../../images/php.png"
+import javascript_logo from "../../images/javascript.png"
+import sql_logo from "../../images/sql.png"
+import html_logo from "../../images/html.png"
+import css_logo from "../../images/css.png"
+import symfony_logo from "../../images/symfony.png"
+import react_logo from "../../images/react.png"
+import codeigniter_logo from "../../images/codeigniter.png"
+import tailwind_logo from "../../images/tailwind.png"
+import bootstrap_logo from "../../images/bootstrap.png"
+import jquery_logo from "../../images/jquery.png"
+import mysql_logo from "../../images/mysql.png"
+import oracle_logo from "../../images/oracle.png"
+import postgre_logo from "../../images/postgres.png"
+import database4_logo from "../../images/oracle.png"
+import git_logo from "../../images/git.png"
+import npm_logo from "../../images/npm.png"
+import composer_logo from "../../images/composer.png"
+import linux_logo from "../../images/linux.png"
+
+export const php = {
+    logo: php_logo,
+    alte: 'PHP'
+} 
