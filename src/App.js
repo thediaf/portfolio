023ffb87fc6ba5c24f6c70809a666bf09192eca3
thediaf <jaharacs98@gmail.com>
2021-12-20 23:35:15 +0000

    Loading screen add

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React, {useState, useEffect} from "react";
+
 import NavBar from './component/Navbar/Navbar';
 import Home from './component/Home/Home';
 import About from './component/About/About';
@@ -6,25 +8,51 @@ import Works from './component/Work/Works';
 import Contact from './component/Contact/Contact';
 import Footer from './component/Footer/Footer';
 import { BrowserRouter } from 'react-router-dom';
+import TwinSpin from "react-cssfx-loading/lib/TwinSpin";
 
 function App() {
+
+  const [loading, setLoading] = useState(true)
+
+//   useEffect(() => {
+//     setTimeout(() => setLoading(false), 3000)
+
+//   }, []);
+
+	const handleLoading = () => {
+		setLoading(false);
+	}
+
+  	useEffect(()=>{
+		window.addEventListener("load",handleLoading);
+		return () => window.removeEventListener("load",handleLoading);
+	},[]);
+
   return (
-    <div className="App">
-      
-      <div className="container mx-auto">
-        <BrowserRouter>
-          <NavBar />
-          <main>
-            <Home />
-            <About />
-            <Skill />
-            <Works />
-            <Contact />
-            <Footer />
-          </main>
-        </BrowserRouter>
-      </div>
-    </div>
+    <>
+    {loading === false ? (
+        <div className="App">
+          <div className="container mx-auto">
+            <BrowserRouter>
+            
+            <NavBar />
+              <main>
+                <Home />
+                <About />
+                <Skill />
+                <Works />
+                <Contact />
+                <Footer />
+              </main>
+            </BrowserRouter>
+          </div>
+        </div>
+     ) : (
+    	<div className="flex h-screen">
+    		<TwinSpin color="rgba(0, 0, 0)" className="m-auto text-gray-900" width="100px" height="100px"  />
+    	</div>
+    )}
+    </>
   );
 }
 
