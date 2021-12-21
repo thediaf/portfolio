32ce67bd7ef19b5658902692d5849f0eec25b0f0
thediaf <jaharacs98@gmail.com>
2021-12-21 18:42:11 +0000

    fix: navItems border deleted

--- src/component/Navbar/NavItem.js
+++ src/component/Navbar/NavItem.js
@@ -10,11 +10,7 @@ const NavItem = ({link, title, closeMobileMenu}) => {
                 smooth={"true"} 
                 duration={500}
                 onClick={closeMobileMenu}
-                className="" 
-                className={isActive =>
-                    "my-3 text-gray-900 cursor-pointer dark:text-gray-200 hover:border-gray-900 dark:hover:border-white md:mx-4 md:my-0 md:border-b-2 transition duration-300 delay-200 ease-linear" + 
-                    (isActive ? " border-gray-800" : "")
-                  }  
+                className="my-3 text-gray-900 cursor-pointer dark:text-gray-200 hover:border-gray-900 dark:hover:border-white md:mx-4 md:my-0 md:border-b-2 transition duration-300 delay-200 ease-linear" 
         >
             {title}
         </NavLink>
