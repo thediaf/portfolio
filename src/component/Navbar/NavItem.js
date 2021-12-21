144bcda75f2c732f961d18dbb71a3698f0c83122
thediaf <jaharacs98@gmail.com>
2021-12-21 23:18:24 +0000

    fix: NavLink scroll bug fixed

--- src/component/Navbar/NavItem.js
+++ src/component/Navbar/NavItem.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-scroll';
 
 const NavItem = ({link, title, closeMobileMenu}) => {
 
     return (
-        <NavLink
+        <Link
                 to={link}
                 spy={"true"}
                 smooth={"true"} 
@@ -13,7 +13,7 @@ const NavItem = ({link, title, closeMobileMenu}) => {
                 className="my-3 text-gray-900 cursor-pointer dark:text-gray-200 hover:border-gray-900 dark:border-gray-800  dark:hover:border-white md:mx-4 md:my-0 md:border-b-2 transition duration-300 delay-200 ease-linear" 
         >
             {title}
-        </NavLink>
+        </Link>
                         
     );
 }
