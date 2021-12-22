90aabc5635882847149047c9c7c9baf38194e920
thediaf <jaharacs98@gmail.com>
2021-12-22 17:58:22 +0000

    fix: load-screen displayed correctly

--- src/App.js
+++ src/App.js
@@ -8,53 +8,26 @@ import Works from './component/Work/Works';
 import Contact from './component/Contact/Contact';
 import Footer from './component/Footer/Footer';
 import { BrowserRouter } from 'react-router-dom';
-import TwinSpin from "react-cssfx-loading/lib/TwinSpin";
-import useDarkMode from "./hook/useDarkMode";
 
 function App() {
 
-  const [colorTheme, setTheme] = useDarkMode();
-  const TwinSpinColor = colorTheme === 'light' ? 'white' : 'black'
-
-  const [loading, setLoading] = useState(true)
-	const handleLoading = () => {
-		setLoading(false);
-	}
-
-  	useEffect(()=>{
-		window.addEventListener("load",handleLoading);
-		return () => window.removeEventListener("load",handleLoading);
-	},[]);
-
-  // useEffect(() => {
-  //   setTimeout(() => setLoading(false), 3000)
-
-  // }, []);
-
   return (
     <>
-        <div className="App">
-    {loading === false ? (
-          <div className="container mx-auto">
-            <BrowserRouter>
-            
-            <NavBar />
-              <main>
-                <Home />
-                <About />
-                <Skill />
-                <Works />
-                <Contact />
-                <Footer />
-              </main>
-            </BrowserRouter>
-        </div>
-     ) : (
-    	<div className="flex h-screen bg-white dark:bg-gray-900">
-    		<TwinSpin color={TwinSpinColor} className="m-auto" width="100px" height="100px"  />
-    	</div>
-    )}
-          </div>
+      <div className="App">
+        <div className="container mx-auto">
+          <BrowserRouter>
+          <NavBar />
+            <main>
+              <Home />
+              <About />
+              <Skill />
+              <Works />
+              <Contact />
+              <Footer />
+            </main>
+          </BrowserRouter>
+        </div>    
+      </div>
 
     </>
   );
